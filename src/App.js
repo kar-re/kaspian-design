442eb92dc2e21ba9b352558075268b09a5e96a5f
Kaspian Jakobsson <kaspianjakobsson@gmail.com>
2022-04-11 16:33:07 +0200

    Changed shake params

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function Rig() {
   const [vec] = useState(() => new THREE.Vector3())
   const { camera, mouse } = useThree()
   useFrame(() => camera.position.lerp(vec.set(mouse.x * 2, 1, 60), 0.05))
-  return <CameraShake maxYaw={0.01} maxPitch={0.01} maxRoll={0.01} yawFrequency={0.5} pitchFrequency={0.5} rollFrequency={0.4} />
+  return <CameraShake maxYaw={0.005} maxPitch={0.005} maxRoll={0.005} yawFrequency={0.2} pitchFrequency={0.5} rollFrequency={0.4} />
 }
 
 export default function App() {
